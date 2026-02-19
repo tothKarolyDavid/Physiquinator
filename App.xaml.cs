5bf095f770a58c24d5a22bcf5765897f4738f567
Tóth Károly Dávid <tothkarolydavid@gmail.com>
2026-02-19 11:06:04 +0100

    chore: Update to .NET 10 and improve Windows deployment
    - Upgrade project from .NET 8 to .NET 10
    
    - Update all package references to version 10.0
    
    - Add WindowsAppSDKSelfContained for standalone deployment
    
    - Fix Blazor WebView localhost address (127.0.0.1 instead of 0.0.0.1)
    
    - Update App.xaml.cs to use CreateWindow pattern

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,11 +1,14 @@
-﻿namespace Physiquinator;
+namespace Physiquinator;
 
 public partial class App : Application
 {
 	public App()
 	{
 		InitializeComponent();
+	}
 
-		MainPage = new MainPage();
+	protected override Window CreateWindow(IActivationState? activationState)
+	{
+		return new Window(new MainPage());
 	}
 }
--- MauiProgram.cs
+++ MauiProgram.cs
@@ -6,6 +6,9 @@ public static class MauiProgram
 {
 	public static MauiApp CreateMauiApp()
 	{
+		// Force Blazor WebView to use 127.0.0.1 instead of 0.0.0.1 (browsers block 0.0.0.x → ERR_ADDRESS_UNREACHABLE).
+		Environment.SetEnvironmentVariable("ASPNETCORE_URLS", "https://127.0.0.1:0");
+
 		var builder = MauiApp.CreateBuilder();
 		builder
 			.UseMauiApp<App>()
