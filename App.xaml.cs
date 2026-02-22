847bf4566ada0b2e79195af10330faf487bef5c3
Tóth Károly Dávid <tothkarolydavid@gmail.com>
2026-02-22 05:33:55 +0100

    Add debug logging for BlazorWebView lifecycle and unhandled exceptions

--- App.xaml.cs
+++ App.xaml.cs
@@ -5,6 +5,17 @@ public partial class App : Application
 	public App()
 	{
 		InitializeComponent();
+
+		AppDomain.CurrentDomain.UnhandledException += (sender, args) =>
+		{
+			System.Diagnostics.Debug.WriteLine($"Unhandled Exception: {args.ExceptionObject}");
+		};
+
+		TaskScheduler.UnobservedTaskException += (sender, args) =>
+		{
+			System.Diagnostics.Debug.WriteLine($"Unobserved Task Exception: {args.Exception}");
+			args.SetObserved();
+		};
 	}
 
 	protected override Window CreateWindow(IActivationState? activationState)
--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -5,5 +5,20 @@ public partial class MainPage : ContentPage
 	public MainPage()
 	{
 		InitializeComponent();
+
+		blazorWebView.BlazorWebViewInitializing += (sender, args) =>
+		{
+			System.Diagnostics.Debug.WriteLine("BlazorWebView Initializing");
+		};
+
+		blazorWebView.BlazorWebViewInitialized += (sender, args) =>
+		{
+			System.Diagnostics.Debug.WriteLine("BlazorWebView Initialized");
+		};
+
+		blazorWebView.UrlLoading += (sender, args) =>
+		{
+			System.Diagnostics.Debug.WriteLine($"URL Loading: {args.Url}");
+		};
 	}
 }
